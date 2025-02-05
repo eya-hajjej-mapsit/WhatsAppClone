ab45c4e28a296ec861ee5d46764189cf88cd29aa
eya.hajej@maps-it.com <eyahajjej9@gmail.com>
2025-02-05 22:45:24 +0100

    Premier commit

---
+++ src/main/java/edu/polytech/whatsapp/common/BaseAuditingEntity.java
@@ -0,0 +1,21 @@
+package edu.polytech.whatsapp.common;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditingEntity {
+}
