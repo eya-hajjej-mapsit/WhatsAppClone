adbb9e98594c80e18868f864d139716e735dacfd
eya-commit <eyahajjej9@gmail.com>
2025-02-05 11:04:34 +0100

    Premier commit

---
+++ src/test/java/edu/polytech/whatsapp/WhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.polytech.whatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
